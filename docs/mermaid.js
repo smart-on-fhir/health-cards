f13cca50bccf626f83e4437f6822e980f99ae7a3
Josh Mandel <jmandel@med.harvard.edu>
2020-08-13 16:46:38 -0500

    Update mermaid render call

--- docs/mermaid.js
+++ docs/mermaid.js
@@ -1,9 +1,6 @@
         var config = {
             startOnLoad:true,
             htmlLabels:true,
-            callback:function(id){
-                console.log(id,' rendered');
-            },
             sequence:{
                     useMaxWidth:false,
                 }
