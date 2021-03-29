a078642c2e1d6aecf8d36b9292f3cc46bf02bdbd
Christian Paquin <cpaquin@microsoft.com>
2021-03-29 12:01:26 -0400

    Added Subject Alternative Name extension to issuer cert.

--- generate-examples/generate-cert-chain.sh
+++ generate-examples/generate-cert-chain.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-# This script generates a 3-cert ECDSA chain (root -> CA -> issuer) valid for 5 years.
+# This script generates a 3-cert ECDSA chain (root -> CA -> issuer) valid for 10 years.
 # Leaf cert uses P-256 (as per the SMART Health Card Framework), CA and root CA use the
-# increasingly stronger P-384 and P-521, respectively.
+# increasingly stronger P-384 and P-521, respectively (simulatate real PKI).
 
 # directory where intermediate files are kept
 tmpdir=certs
 mkdir -p $tmpdir
 
 # generate self-signed root CA cert
-openssl req -x509 -new -newkey ec:<(openssl ecparam -name secp521r1) -keyout $tmpdir/root_CA.key -out $tmpdir/root_CA.crt -nodes -subj "/CN=SMART Health Card Example Root CA" -days 1825
+openssl req -x509 -new -newkey ec:<(openssl ecparam -name secp521r1) -keyout $tmpdir/root_CA.key -out $tmpdir/root_CA.crt -nodes -subj "/CN=SMART Health Card Example Root CA" -days 3650 -config openssl_ca.cnf -extensions v3_ca
 
 # generate intermediate CA cert request
-openssl req -new -newkey ec:<(openssl ecparam -name secp384r1) -keyout $tmpdir/CA.key -out $tmpdir/CA.csr -nodes -subj "/CN=SMART Health Card Example CA" -addext basicConstraints=critical,CA:true
+openssl req -new -newkey ec:<(openssl ecparam -name secp384r1) -keyout $tmpdir/CA.key -out $tmpdir/CA.csr -nodes -subj "/CN=SMART Health Card Example CA" -config openssl_ca.cnf -extensions v3_ca
 
 # root CA signs the CA cert request
-openssl x509 -req -in $tmpdir/CA.csr -out $tmpdir/CA.crt -CA $tmpdir/root_CA.crt -CAkey $tmpdir/root_CA.key -CAcreateserial -days 1825
+openssl x509 -req -in $tmpdir/CA.csr -out $tmpdir/CA.crt -CA $tmpdir/root_CA.crt -CAkey $tmpdir/root_CA.key -CAcreateserial -days 3650 -extfile openssl_ca.cnf -extensions v3_ca
 
 # generate issuer signing cert request
-openssl req -new -newkey ec:<(openssl ecparam -name prime256v1) -keyout $tmpdir/issuer.key -out $tmpdir/issuer.csr -nodes -subj "/CN=SMART Health Card Example Issuer" -addext keyUsage=digitalSignature
+openssl req -new -newkey ec:<(openssl ecparam -name prime256v1) -keyout $tmpdir/issuer.key -out $tmpdir/issuer.csr -nodes -subj "/CN=SMART Health Card Example Issuer" -config openssl_ca.cnf -extensions v3_req
 
 # intermediate CA signs the issuer cert request
-openssl x509 -req -in $tmpdir/issuer.csr -out $tmpdir/issuer.crt -CA $tmpdir/CA.crt -CAkey $tmpdir/CA.key -CAcreateserial -days 1825
+openssl x509 -req -in $tmpdir/issuer.csr -out $tmpdir/issuer.crt -CA $tmpdir/CA.crt -CAkey $tmpdir/CA.key -CAcreateserial -days 3650 -extfile openssl_ca.cnf -extensions v3_req
+
+# add the issuer key to the JWK sets 
 node src/certs-to-x5c.js --key $tmpdir/issuer.key --cert $tmpdir/issuer.crt --cert $tmpdir/CA.crt --cert $tmpdir/root_CA.crt --private src/config/issuer.jwks.private.json --public issuer/.well-known/jwks.json
